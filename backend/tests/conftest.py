f43e44485f2c90b195d1f048de5c1896b8f85346
RitaAse <rita.asemota01@gmail.com>
2026-07-23 02:13:57 +0200

    Configure CI with PostgreSQL test database

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,21 @@
 import pytest
 
 from app.main import app
+from app.db.base import Base
+from app.db.database import engine
 from app.services.storage_service import get_storage
 
 from tests.fakes.fake_storage import FakeStorage
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_database():
+
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    Base.metadata.drop_all(bind=engine)
 
 
 @pytest.fixture
